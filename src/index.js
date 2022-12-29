84688c63fdaa05e14166ecbfec8a287e4e17c810
tobuya <thomasobuya95@yahoo.com>
2022-12-29 10:58:34 +0300

    Update luxon date function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const dateHolder = document.getElementById('luxon-date');
 const displayDate = () => {
     const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
     dateHolder.innerHTML = now;
-}
+};
 
 window.addEventListener('load', displayDate);
