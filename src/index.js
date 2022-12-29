462ef972a034de535947ca8433cd14269f45edde
tobuya <thomasobuya95@yahoo.com>
2022-12-29 22:49:13 +0300

    Import modules files function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import { DateTime } from "luxon";
 import './index.css';
+import { showBookList } from './modules/book-list.js';
+import { addNewBook } from './modules/new-book.js';
+import { showContact } from './modules/contact.js';
 
 const dateHolder = document.getElementById('luxon-date');
+export const mainSection = document.getElementById('main');
+export const listSection = document.getElementById('list');
+export const addNewBookSection = document.getElementById('add');
+export const contactSection = document.getElementById('contact');
 
 const displayDate = () => {
     const now = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
@@ -9,3 +16,6 @@ const displayDate = () => {
 };
 
 window.addEventListener('load', displayDate);
+listSection.addEventListener('click', showBookList);
+addNewBookSection.addEventListener('click', addNewBook);
+contactSection.addEventListener('click', showContact);
